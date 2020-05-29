981b87f5b3c7dce9120d34c0481dc2133f7bdcd2
Smruthi Balajee <smruthibalajee@gmail.com>
2020-05-28 18:23:32 -0700

    Navbar is complete

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -54,5 +54,17 @@ function writeName() {
     writeText(name, "hero-text");
 }
 
+/** Hides the navbar when scrolling down, nav bar appears when scrolling up. */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 
 
